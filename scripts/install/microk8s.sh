236f9f491df202ccb99f1ef88ec60bc525e743bf
Carlos Garcia <carlos.garcia@scrm.eu>
2021-01-01 23:30:48 +0100

    add scripts

---
+++ scripts/install/microk8s.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+sudo apt install snapd -fy
+sudo snap install microk8s --classic --channel=1.19
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+echo "export PATH=$PATH:/snap/bin" >> ~/.profile
+echo "alias kubectl='microk8s kubectl'" >> ~/.bash_aliases
+.  ~/.bash_aliases
+microk8s status --wait-ready
+microk8s enable dns storage ingress rbac registry
