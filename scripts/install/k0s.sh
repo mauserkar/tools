812bd90033a2a730b9319a4de024a2e6925a9145
Carlos Garcia <carlos.garcia@scrm.lidl>
2021-11-15 15:47:16 +0100

    update k0s k0s script

--- scripts/install/k0s.sh
+++ scripts/install/k0s.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-PUBLIC_IP=$(curl -s ifconfig.co)
+PUBLIC_IP=$(curl -s ifconfig.co) \
 PRIVATE_IP=$(hostname -I | awk '{print $1}')
 curl -sSLf https://get.k0s.sh | sudo sh
 sudo k0s default-config > k0s-cluster-config.yml
-sudo k0s kubeconfig admin > k0s.kubectl.yml
 cp k0s-cluster-config.yml k0s-cluster-config.yml.bk
-sudo sed -i 's/${PRIVATE_IP}/${PUBLIC_IP}/g' k0s-cluster-config.yml
-sudo sed -i 's/${PRIVATE_IP}/${PUBLIC_IP}/g' k0s.kubectl.yml
+sudo sed -i "s/$PRIVATE_IP/$PUBLIC_IP/g" k0s-cluster-config.yml
 sudo k0s install controller --enable-worker -c k0s-cluster-config.yml
 sudo k0s start
-echo "alias kc='sudo k0s kubectl'" >> ~/.bash_aliases
-source ~/.bash_aliases
+sudo k0s kubeconfig admin > k0s-kubectl-config.yml
+sudo sed -i "s/$PRIVATE_IP/$PUBLIC_IP/g" k0s-kubectl-config.yml
+cat k0s-kubectl-config.yml
